75dc83f9713b188acf9d5c6c179eeb8ecf01836b
samuel <samuelborzi815@gmail.com>
2022-01-17 11:12:51 +0200

    harjoitukset 1, 2 ja 3

---
+++ Harjoitus 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Matti", "TVT1234", 0);
+
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+            
+        }
+    }
+    
+
+}
---
+++ Harjoitus/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo auto = new Ajoneuvo();
+
+            auto.Nimi = "Toyota";
+            auto.Nopeus = 160;
+            auto.Renkaat = 4;
+            
+            auto.TulostaData();
+
+            string autonTiedot = auto.ToString();
+
+            Console.WriteLine(autonTiedot);
+
+            Console.WriteLine();
+
+            Ajoneuvo mopo = new Ajoneuvo();
+            mopo.Nimi = "Skootteri ";
+            mopo.Nopeus = 55;
+            mopo.Renkaat = 2;
+
+            mopo.TulostaData();
+            Console.WriteLine(mopo.ToString());
+        }
+    }
+}
