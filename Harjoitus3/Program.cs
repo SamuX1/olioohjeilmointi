b07c5c88ea78dd667a600d3ac9d64c5f24f96ec1
samuel <samuelborzi815@gmail.com>
2022-01-18 10:48:56 +0200

    harjoitus3 Commit

--- Harjoitus3/Program.cs
+++ Harjoitus3/Program.cs
@@ -10,6 +10,19 @@ namespace Harjoitus3
     {
         static void Main(string[] args)
         {
+
+            kiuas harvia = new kiuas("Harvia", 80, 50);
+
+            harvia.HaeTiedot();
+            harvia.Tila = true;
+            harvia.HaeTiedot();
+
+            harvia.Lampotila = 90;
+            harvia.Kosteus = 60;
+
+            harvia.HaeTiedot();
+            harvia.Tila = false;
+            harvia.HaeTiedot();
         }
     }
 }
--- Harjoitus3/kiuas.cs
+++ Harjoitus3/kiuas.cs
@@ -8,5 +8,33 @@ namespace Harjoitus3
 {
     class kiuas
     {
+        public string Merkki;
+        public int Lampotila;
+        public int Kosteus;
+        public int Tila;
+
+        public kiuas(string merkki, int lampotila, int kosteus)
+        {
+            Merkki = merkki;
+            Lampotila = lampotila;
+            Kosteus = kosteus;
+        }
+
+        public void HaeTiedot()
+        {
+            Console.WriteLine("");
+            if (Tila == true) 
+            {
+                Console.WriteLine("Kiuas " + Merkki + " on päällä");
+                Console.WriteLine(" -Lämpötila " + Lampotila);
+                Console.WriteLine(" -Kosteus " + Kosteus);
+        
+
+            }
+            else
+            {
+                Console.WriteLine("Kiuas " + Merkki + " ei ole päällä");
+            }
+        }
     }
 }
