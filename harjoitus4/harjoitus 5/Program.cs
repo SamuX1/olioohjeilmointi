3d7f969272a67d9250533cd0696f164d1d9c4262
samuel <samuelborzi815@gmail.com>
2022-01-18 13:28:34 +0200

    harjoitus5 Commit

---
+++ harjoitus4/harjoitus 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float a = 5.0f;
+            float b = 2.5f;
+
+            float summa = Laskin.Summa(a, b);
+            float erotus = Laskin.Erotus(a, b);
+            float kertolasku = Laskin.Kertolasku(a, b);
+            float jakolasku = Laskin.Jakolasku(a, b);
+
+            Console.WriteLine("Numeroiden {0} ja {1}:", a, b);
+            Console.WriteLine("Summa: " + summa);
+            Console.WriteLine("Erotus: " + erotus);
+            Console.WriteLine("Kertolasku: " + kertolasku);
+            Console.WriteLine("Jakolasku: " + jakolasku);
+
+
+
+        }
+    }
+}
