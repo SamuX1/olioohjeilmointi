bafe25efba328fec32de17e31c2e7f97fa8951f1
samuel <samuelborzi815@gmail.com>
2022-01-31 12:34:09 +0200

    harjoitus 11 Commit

---
+++ harjoitus4/Harjoitus11/Opiskelija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus11
+{
+    class Opiskelija
+    {
+        public Opiskelija(string a, string b, string c, string d)
+        {
+
+        }
+        public string opiskelijaID { get; set; }
+
+
+    }
+}
---
+++ harjoitus4/Harjoitus11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Opiskelija> opiskelijaominaisuudet = new Dictionary<string, Opiskelija>();
+
+            Opiskelija keppo = new Opiskelija("Keppa", "Keppanen", "ryhmatunnus", "12383-3213");
+            Opiskelija pikko = new Opiskelija("Pikko", "Pikkonen", "ryhmatunnus", "12378-2173");
+
+            opiskelijaominaisuudet.Add(keppo.opiskelijaID, keppo);
+            opiskelijaominaisuudet.Add(pikko.opiskelijaID, pikko);
+
+            opiskelijaominaisuudet.Remove("12378-2173");
+
+            try
+            {
+                foreach (string opi in opiskelijaominaisuudet.Keys)
+                {
+                    Console.WriteLine("Henkilön tiedot: ");
+                    Console.WriteLine("--Opiskelijan ominaisuudet: " + opiskelijaominaisuudet);
+                    Console.WriteLine("--OpiskelijaID: " + opi);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
